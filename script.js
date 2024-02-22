1d962f8a1a1a06a989ece22e7681e476e46e4e3a
Ali N. Abadian <nagshrizali@gmail.com>
2024-02-22 22:23:59 +0330

    6

--- script.js
+++ script.js
@@ -1,32 +1,24 @@
 const min = 1;
 const max = 100;
 const answer = Math.floor(Math.random() * (max - min + 1)) + min;
-let input = document.getElementById("numberToGuess").value;
-
 let attempts = 0;
-// let guess = input;
 let running = true;
 
 function startGame() {
-  console.log(input);
+  const input = parseInt(document.getElementById("numberToGuess").value);
+  if (isNaN(input)) {
+    window.alert("Please enter a valid number!");
+    return;
+  } else if (input < min || input > max) {
+    window.alert("Please enter a valid number!");
+  } else {
+    attempts++;
+    if (input < answer) {
+      window.alert("Too Low");
+    } else if (input > answer) {
+      window.alert("Too High");
+    } else {
+      window.alert(`CORRECT! Attempts ${attempts}.`);
+    }
+  }
 }
-
-// while (running) {
-//   guess = input;
-//   guess = Number(guess);
-
-//   //   if (isNaN(guess)) {
-//   //     window.alert("Please enter a valid number!");
-//   //   } else if (guess < min || guess > max) {
-//   //     window.alert("Please enter a valid number!");
-//   //   } else {
-//   //     attempts++;
-//   //     if (guess < answer) {
-//   //       window.alert("Too Low");
-//   //     } else if (guess > answer) {
-//   //       window.alert("Too High");
-//   //     } else {
-//   //       window.alert(`CORRECT! Attempts ${attempts}.`);
-//   //       running = false;
-//   //     }
-// }
